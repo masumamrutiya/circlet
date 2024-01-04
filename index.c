9b034307a892a38b812a2eace4428474a902cff4
masumamrutiya <masumamrutiya29@gmail.com>
2024-01-04 11:37:15 +0530

    hello

---
+++ index.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main(){
+
+    // ---------------- question-1-----------------
+    for(int i=1 ; i<=8 ;i++){
+        printf("* ");
+    }
+    printf("\n");
+
+     for(int i=1 ; i<=8 ;i++){
+        printf("* ");
+    }
+    printf("\n");
+    
+     for(int i=1 ; i<=8 ;i++){
+        printf("* ");
+    }
+    printf("\n");
+    
+     for(int i=1 ; i<=8 ;i++){
+        printf("* ");
+    }
+    printf("\n");
+     for(int i=1 ; i<=8 ;i++){
+        printf("* ");
+    }
+    printf("\n");
+
+
+
+
+// ------------------question-2------------------ 
+
+
+
+
+
+
+
+    return 0;
+}
